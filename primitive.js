b0a7c346d4dc9ac939acc8fdab7fd34ddc5df125
Mariusz Nowak <mariusz@medikoo.com>
2013-11-22 18:34:56 +0100

    Observable primitive

---
+++ primitive.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = require('./to-array')(require('./and-or-not')(
+	require('./filter-map')(require('./create')(require('es6-set/primitive')))
+));
---
+++ test/primitive.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var remove    = require('es5-ext/array/#/remove')
+  , isSet     = require('es6-set/is-set')
+  , toArray   = require('es6-iterator/to-array')
+  , andOrNot  = require('./and-or-not').tests;
+
+module.exports = function (ObservableSet, a) {
+	var arr = ['foo', 'bar', '23'].sort()
+	  , set = new ObservableSet(arr), adds = 0, deletes = 0, clears = 0;
+
+	a(isSet(set), true, "Is set");
+	a(set instanceof ObservableSet, true, "Subclassed");
+
+	a.deep(toArray(set.values()), arr, "Constructor");
+
+	set.on('change', function (event) {
+		if (event.type === 'add') ++adds;
+		else if (event.type === 'delete') ++deletes;
+		else ++clears;
+	});
+
+	set.delete('bar');
+	remove.call(arr, 'bar');
+
+	a.deep(toArray(set.values()), arr, "Delete: value");
+	a.deep([adds, deletes, clears], [0, 1, 0], "Delete: event");
+
+	set.delete('elo');
+	a.deep(toArray(set.values()), arr, "Delete non existing: value");
+	a.deep([adds, deletes, clears], [0, 1, 0], "Delete non existing: event");
+
+	set.add('muszka');
+	arr.push('muszka');
+	a.deep(toArray(set.values()), arr, "Add: value");
+	a.deep([adds, deletes, clears], [1, 1, 0], "Add: event");
+
+	set.add('muszka');
+	a.deep(toArray(set.values()), arr, "Add same: value");
+	a.deep([adds, deletes, clears], [1, 1, 0], "Add same: event");
+
+	set.clear();
+	a.deep(toArray(set.values()), [], "Clear: value");
+	a.deep([adds, deletes, clears], [1, 1, 1], "Clear: event");
+
+	set.clear();
+	a.deep(toArray(set.values()), [], "Clear on empty: value");
+	a.deep([adds, deletes, clears], [1, 1, 1], "Clear on empty: event");
+
+	andOrNot(ObservableSet, a);
+};
