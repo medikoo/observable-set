aa8af0802b057270f3e285dfba7afc04d369ca5e
Mariusz Nowak <medyk@medikoo.com>
2015-03-25 17:12:29 +0100

    Support observable input in has

--- has.js
+++ has.js
@@ -4,6 +4,7 @@ var d                 = require('d')
   , memoize           = require('memoizee/plain')
   , memoizeMethods    = require('memoizee/methods-plain')
   , getNormalizer     = require('memoizee/normalizers/get-1')
+  , isObservable      = require('observable-value/is-observable-value')
   , ReadOnly          = require('observable-value/create-read-only')(require('observable-value'))
   , validObservableSet = require('./valid-observable-set')
 
@@ -14,8 +15,15 @@ module.exports = memoize(function (prototype) {
 
 	return defineProperties(prototype, memoizeMethods({
 		_has: d(function (value) {
-			var result = new ReadOnly(this.has(value));
-			this.on('change', function () { result._setValue(this.has(value)); });
+			var result, onChange;
+			if (isObservable(value)) {
+				result = new ReadOnly(this.has(value.value));
+				this.on('change', onChange = function () { result._setValue(this.has(value.value)); });
+				value.on('change', onChange);
+			} else {
+				result = new ReadOnly(this.has(value));
+				this.on('change', function () { result._setValue(this.has(value)); });
+			}
 			return result;
 		}, { getNormalizer: getNormalizer })
 	}));
